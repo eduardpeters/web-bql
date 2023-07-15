f959d358f68efa5cfa6014f00a76a676d33130b1
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-07-15 19:59:00 +0200

    Add BlockDisplay component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import BlockDisplay from "./BlockDisplay.svelte";
+
     let queryElements: string[] = [];
 
     const handleDrag = (event: DragEvent) => {
@@ -22,11 +24,7 @@
 
 <h1>Welcome to Block Query Language</h1>
 <main class="container">
-    <div class="keywords_holder">
-        {#each ["SELECT", "FROM", "WHERE"] as keyword}
-            <p draggable="true" on:dragstart={handleDrag}>{keyword}</p>
-        {/each}
-    </div>
+    <BlockDisplay handleDrag={handleDrag} />
     <div
         role="textbox"
         class="queries_holder"
@@ -49,15 +47,6 @@
         flex-direction: row;
         justify-content: space-around;
         min-height: 300px;
-    }
-
-    .keywords_holder {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        border: 1px solid black;
-        min-width: 400px;
     }
 
     .queries_holder {
---
+++ src/routes/BlockDisplay.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let handleDrag: (event: DragEvent) => void;
+</script>
+
+<div class="keywords_holder">
+    {#each ["SELECT", "FROM", "WHERE"] as keyword}
+        <p draggable="true" on:dragstart={handleDrag}>{keyword}</p>
+    {/each}
+</div>
+
+<style>
+    .keywords_holder {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid black;
+        min-width: 400px;
+    }
+</style>
