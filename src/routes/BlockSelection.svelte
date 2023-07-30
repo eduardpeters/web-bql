b818a448e750c919f8cb906059bc6e75d0ddf9f9
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-07-30 19:19:59 +0200

    Add buttons to BlockSelection

--- src/routes/BlockSelection.svelte
+++ src/routes/BlockSelection.svelte
@@ -4,22 +4,46 @@
     import { tablesAndColumns } from "$lib/stores/dbStore";
     import { keywords } from "$lib/keywords/keywords";
 
+    enum BlockOptions {
+        Keywords,
+        TablesAndColumns,
+        Symbols,
+    }
+
     let blocksToDisplay = keywords;
+    let blocksSelected = BlockOptions.Keywords;
 
-    const handleShowKeywords = () => {
-        blocksToDisplay = keywords;
+    $: {
+        if (
+            $tablesAndColumns &&
+            blocksSelected === BlockOptions.TablesAndColumns
+        )
+            blocksToDisplay = $tablesAndColumns;
     }
 
-    const handleShowTables = () => {
-        blocksToDisplay = $tablesAndColumns;
-    }
+    const handleSelection = (selection: BlockOptions) => {
+        blocksSelected = selection;
+        if (blocksSelected === BlockOptions.Keywords) {
+            blocksToDisplay = keywords;
+        } else if (blocksSelected === BlockOptions.TablesAndColumns) {
+            blocksToDisplay = $tablesAndColumns;
+        } else if (blocksSelected === BlockOptions.Symbols) {
+            console.log("No Symbols set yet");
+        }
+    };
 </script>
 
 <div class="container">
-    <div>
-        <button on:click={handleShowKeywords}>SQL Keywords</button>
-        <button on:click={handleShowTables}>Tables and Columns</button>
-        <button>Numbers and Symbols</button>
+    <div class="selection_controls">
+        <button on:click={() => handleSelection(BlockOptions.Keywords)}>
+            SQL Keywords
+        </button>
+        <button on:click={() => handleSelection(BlockOptions.TablesAndColumns)}>
+            Tables and Columns
+        </button>
+        <button on:click={() => handleSelection(BlockOptions.Symbols)}>
+            Numbers and Symbols
+        </button>
     </div>
     <BlockDisplay blocks={blocksToDisplay} />
 </div>
