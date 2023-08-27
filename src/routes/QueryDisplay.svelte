f9769be8c1f07aa3bf5ba7028a9223b7f5fcddd0
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-08-27 19:07:42 +0200

    Remove original DND in QueryDisplay

--- src/routes/QueryDisplay.svelte
+++ src/routes/QueryDisplay.svelte
@@ -4,7 +4,6 @@
     import Block from "$lib/components/Block.svelte";
 
     export let queryElements: BlockContent[];
-    let draggedElement: { content: BlockContent; removed: boolean } | null;
 
     const handleConsider = (event: any) => {
         console.log(event);
@@ -15,68 +14,8 @@
         console.log(event);
         queryElements = event.detail.items;
     };
-
-    const handleDragStart = (event: DragEvent, content: BlockContent) => {
-        if (!event || !event.dataTransfer) return;
-        event.dataTransfer.setData("text/plain", JSON.stringify(content));
-        event.dataTransfer.dropEffect = "move";
-        draggedElement = { content, removed: false };
-    };
-
-    const handleDragEnd = (event: DragEvent, content: BlockContent) => {
-        if (!event || !event.dataTransfer) return;
-        if (event.dataTransfer.dropEffect === "none") {
-            queryElements = queryElements.filter(
-                (element) => element !== content
-            );
-        }
-        draggedElement = null;
-    };
-
-    const handleDragOver = (event: DragEvent) => {
-        if (!event || !event.dataTransfer) return;
-        event.dataTransfer.dropEffect = "copy";
-    };
-
-    const handleDrop = (event: DragEvent) => {
-        if (!event || !event.dataTransfer) return;
-        if (draggedElement) return;
-        const data = JSON.parse(event.dataTransfer.getData("text/plain"));
-        queryElements = [...queryElements, data];
-    };
-
-    const handleDragEnter = (event: DragEvent) => {
-        if (!event || !event.dataTransfer || !draggedElement) return;
-        draggedElement.removed = false;
-    };
-
-    const handleDragLeave = (event: DragEvent) => {
-        if (!event || !event.dataTransfer || !draggedElement) return;
-        draggedElement.removed = true;
-    };
 </script>
 
-<div class="container">
-    <div
-        role="textbox"
-        class="query_holder"
-        tabindex="-1"
-        on:dragover|preventDefault={handleDragOver}
-        on:drop|preventDefault={handleDrop}
-        on:dragenter|preventDefault={handleDragEnter}
-        on:dragleave|preventDefault={handleDragLeave}
-    >
-        {#if queryElements.length > 0}
-            {#each queryElements as element}
-                <Block content={element} {handleDragStart} {handleDragEnd} />
-            {/each}
-        {:else}
-            <p class="placeholder">Drop some blocks here to begin</p>
-        {/if}
-    </div>
-    <button on:click={() => (queryElements = [])}>RESET</button>
-</div>
-<p>DND re-implementation</p>
 <div class="container">
     {#if queryElements.length <= 0}
         <p class="placeholder">Drop some blocks here to begin</p>
@@ -120,7 +59,7 @@
     }
 
     .placeholder {
-        color: #eee;
+        color: #003554;
         font-family: monospace;
     }
 
@@ -131,6 +70,7 @@
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
+        min-height: 80%;
         justify-content: flex-start;
         padding: 10px;
         width: 80%;
