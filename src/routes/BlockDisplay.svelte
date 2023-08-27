68cc3d71f1350fe1280cafcc0eadd4be0584d4d3
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-08-27 19:04:57 +0200

    Remove original DND in BlockDisplay

--- src/routes/BlockDisplay.svelte
+++ src/routes/BlockDisplay.svelte
@@ -5,12 +5,6 @@
 
     export let blocks: BlockContent[];
 
-    const handleDragStart = (event: DragEvent, content: BlockContent) => {
-        if (!event || !event.dataTransfer) return;
-        event.dataTransfer.setData("text/plain", JSON.stringify(content));
-        event.dataTransfer.dropEffect = "copy";
-    };
-
     const handleConsider = (event: any) => {
         console.log(event);
         blocks = event.detail.items;
@@ -22,17 +16,6 @@
     };
 </script>
 
-<div class="keywords_holder">
-    {#each blocks as block}
-        <Block content={block} {handleDragStart} />
-        {#if block.columns}
-            {#each block.columns as column}
-                <Block content={column} {handleDragStart} />
-            {/each}
-        {/if}
-    {/each}
-</div>
-<p>DND re-implementation</p>
 <div
     class="keywords_holder"
     use:dndzone={{ items: blocks }}
@@ -43,7 +26,7 @@
         <Block content={block} />
         {#if block.columns}
             {#each block.columns as column}
-                <Block content={column} {handleDragStart} />
+                <Block content={column} />
             {/each}
         {/if}
     {/each}
