f2772f24bb8dda9ed17671da1f6a93de1f20eee6
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-08-27 18:28:06 +0200

    Add ID to symbols

--- src/lib/keywords/symbols.ts
+++ src/lib/keywords/symbols.ts
@@ -1,57 +1,71 @@
 export const symbols = [
     {
+        id: "TKor0",
         name: "*",
         type: "column",
     },
     {
+        id: "GNBeh",
         name: "0",
         type: "symbol",
     },
     {
+        id: "MIwiC",
         name: "1",
         type: "symbol",
     },
     {
+        id: "gKj6m",
         name: "2",
         type: "symbol",
     },
     {
+        id: "cP51T",
         name: "3",
         type: "symbol",
     },
     {
+        id: "5KRez",
         name: "4",
         type: "symbol",
     },
     {
+        id: "zDnDj",
         name: "5",
         type: "symbol",
     },
     {
+        id: "pbBQJ",
         name: "6",
         type: "symbol",
     },
     {
+        id: "2nwTu",
         name: "7",
         type: "symbol",
     },
     {
+        id: "ZbmRY",
         name: "8",
         type: "symbol",
     },
     {
+        id: "yFqwv",
         name: "9",
         type: "symbol",
     },
     {
+        id: "qAXrk",
         name: "=",
         type: "symbol",
     },
     {
+        id: "4Z3gv",
         name: "<",
         type: "symbol",
     },
     {
+        id: "Ovkoo",
         name: ">",
         type: "symbol",
     },
