30ac2c2a87233bb5fdcec18e68ecfcc5a7c41719
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-09-09 17:38:42 +0200

    Add BlockTypes enum to queryParser

--- src/lib/utils/queryParser.ts
+++ src/lib/utils/queryParser.ts
@@ -1,21 +1,18 @@
-import type { BlockContent } from "$lib/appTypes";
+import { BlockTypes, type BlockContent } from "$lib/appTypes";
 
-const T_SYMBOL = 'symbol';
-const T_NUMBER = 'number';
 /*
 Block types that require special handling are symbol and numbers
 It is currently easier to have each non-symbol, non-number append a space before
 and the symbol or number not to to avoid separating digits or operators for example
 */
-
 const queryParser = (queryElements: BlockContent[]): string => {
     if (queryElements.length < 1) return '';
     let queryString = queryElements[0].name;
     for (let i = 1; i < queryElements.length; i++) {
         if (
             (queryElements[i].type !== queryElements[i - 1].type)
-            || (queryElements[i].type !== T_SYMBOL && queryElements[i].type !== T_NUMBER)
-            || (queryElements[i - 1].type !== T_SYMBOL && queryElements[i - 1].type !== T_NUMBER)
+            || (queryElements[i].type !== BlockTypes.symbol && queryElements[i].type !== BlockTypes.number)
+            || (queryElements[i - 1].type !== BlockTypes.symbol && queryElements[i - 1].type !== BlockTypes.number)
         ) {
             queryString += ' ';
         }
