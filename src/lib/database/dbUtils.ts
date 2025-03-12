06f4ef67e438c168ea81c223a6afb3c0f3c33ef0
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-03-12 21:22:15 +0100

    refactor: add return type signatures

--- src/lib/database/dbUtils.ts
+++ src/lib/database/dbUtils.ts
@@ -1,12 +1,14 @@
 import { BlockTypes, type BlockContent } from '$lib/types/Blocks';
 import query from './connection';
 
-export const getDbBlocks = async (dbFile: ArrayBuffer) => {
+export const getDbBlocks = async (dbFile: ArrayBuffer): Promise<BlockContent[]> => {
 	const blocks: BlockContent[] = [];
+
 	const tables = await getTables(dbFile);
 	if (!tables) {
 		return blocks;
 	}
+
 	for (let i = 0; i < tables.length; i++) {
 		blocks.push({
 			id: `${tables[i]}-t-${Math.round(Math.random() * 1000)}`,
@@ -25,11 +27,13 @@ export const getDbBlocks = async (dbFile: ArrayBuffer) => {
 			});
 		}
 	}
+
 	return blocks;
 };
 
-export const getTables = async (dbFile: ArrayBuffer) => {
+export const getTables = async (dbFile: ArrayBuffer): Promise<string[] | undefined> => {
 	const queryString = "SELECT name FROM sqlite_master WHERE type = 'table'";
+
 	const result = await query(dbFile, queryString);
 	if (result) {
 		const tableNames = result.rows.map((name) => name[0]);
@@ -37,8 +41,9 @@ export const getTables = async (dbFile: ArrayBuffer) => {
 	}
 };
 
-export const getColumns = async (dbFile: ArrayBuffer, tableName: string) => {
+export const getColumns = async (dbFile: ArrayBuffer, tableName: string): Promise<string[] | undefined> => {
 	const queryString = `SELECT * FROM ${tableName} LIMIT 0`;
+
 	const result = await query(dbFile, queryString);
 	if (result) {
 		const columns = result.columns;
